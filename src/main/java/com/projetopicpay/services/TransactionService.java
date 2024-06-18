342b01ecfcd405aa3708dd160a7f08ad83d41e62
Maikon <mknsposito@gmail.com>
2024-06-17 21:28:34 -0300

    create TransactionService

---
+++ src/main/java/com/projetopicpay/services/TransactionService.java
@@ -0,0 +1,63 @@
+package com.projetopicpay.services;
+
+import com.projetopicpay.DTO.TransactionDTO;
+import com.projetopicpay.domain.transaction.Transaction;
+import com.projetopicpay.domain.user.User;
+import com.projetopicpay.repositories.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Service
+public class TransactionService {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private TransactionRepository repository;
+
+    @Autowired
+    private RestTemplate restTemplate; //Classe do Spring para fazer chamadas HTTP
+
+    public void createTransaction(TransactionDTO transaction) throws Exception {
+        User sender = this.userService.findUserById(transaction.senderId());
+        User receiver = this.userService.findUserById(transaction.receiveriD());
+
+        userService.validateTransaction(sender, transaction.value());
+
+        boolean isAuthorized = this.authorizeTransaction(sender, transaction.value());
+        if (!isAuthorized){
+            throw new Exception("Transação não autorizada");
+        }
+
+        Transaction transaction1 = new Transaction();
+        transaction1.setAmount(transaction.value());
+        transaction1.setSender(sender);
+        transaction1.setReceiver(receiver);
+        transaction1.setTimestamp(LocalDateTime.now());
+
+        sender.setBalance(sender.getBalance().subtract(transaction.value()));
+        receiver.setBalance(receiver.getBalance().add(transaction.value()));
+
+        this.repository.save(transaction1);
+        this.userService.saveUser(sender);
+        this.userService.saveUser(receiver);
+    }
+
+    public boolean authorizeTransaction(User sender, BigDecimal value){
+      ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity("https://util.devi.tools/api/v2/authorize", Map.class);
+
+      if (authorizationResponse.getStatusCode() == HttpStatus.OK){
+          String message = (String) authorizationResponse.getBody().get("message");
+          return "Autorizado".equalsIgnoreCase(message);
+      }else return false;
+    }
+
+
+}
