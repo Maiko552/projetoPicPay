304773d7114075131fe635d52cdf244442bf1386
Maiko552 <mknsposito@gmail.com>
2024-06-18 00:10:20 -0300

    final

--- src/main/java/com/projetopicpay/ProjetoPicpayApplication.java
+++ src/main/java/com/projetopicpay/ProjetoPicpayApplication.java
@@ -3,6 +3,7 @@ package com.projetopicpay;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Maikon Sposito
 @SpringBootApplication
 public class ProjetoPicpayApplication {
 
